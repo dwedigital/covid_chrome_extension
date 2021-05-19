7d4286c7d1bc30068f4ce95f5247bca9e4024e2e
Dave Edwards <daveedwards@Daves-MacBook-Pro-13-Inch.local>
2021-05-19 12:33:13 +0100

    added in latest case increase

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
-    fetch('http://127.0.0.1:8000/active-cases')
+    fetch('https://vibrant-grammar-254613.ew.r.appspot.com/active-cases')
         .then(data => data.json())
         .then((data) => {
             document.querySelector('.date').innerHTML = data.date;
             document.querySelector('.active-data').innerHTML = data.active;
-            document.querySelector('.new-cases').innerHTML = data.newCases;
+            document.querySelector('.latest-cases-data').innerHTML = data.newCases;
         })
 }
